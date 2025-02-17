452e2bbc9264ee3d11c8bbc0fd6520005c7732fc
OverjoyedMaxwel <kanapod.lam@student.mahidol.edu>
2025-02-17 10:38:18 +0000

    submit

--- ll_example.c
+++ ll_example.c
@@ -2,14 +2,16 @@
 // Inserting and deleting nodes in a list
 #include <stdio.h>
 #include <stdlib.h>
-#include "ll.h"
-
+#include <string.h>
+//#include "ll.h"
+#include "ll2.h"
 int main( void )
 { 
    LLPtr startPtr = NULL; // initially there are no nodes
    unsigned int choice; // user's choice
    int item; // char entered by user
-
+   char name[50];
+   
    instructions(); // display the menu
    printf( "%s", "? " );
    scanf( "%u", &choice );
@@ -19,24 +21,24 @@ int main( void )
 
       switch ( choice ) { 
          case 1:
-            printf( "%s", "Enter a number: " );
-            scanf( "%d", &item );
-            insert( &startPtr, item ); // insert item in list
+            printf( "%s", "Enter id and name: " );
+            scanf( "%d %s", &item, name);
+            insert( &startPtr, item , name); // insert item in list
             printList( startPtr );
             break;
          case 2: // delete an element
             // if list is not empty
             if ( !isEmpty( startPtr ) ) { 
                printf( "%s", "Enter number to be deleted: " );
-               scanf( "%d", &item );
+               scanf( "%d", &item);
 
                // if character is found, remove it
-               if ( deletes( &startPtr, item ) ) { // remove item
-                  printf( "%d deleted.\n", item );
+               if ( deletes( &startPtr, item) ) { // remove item
+                  printf( "%d deleted.\n", item);
                   printList( startPtr );
                } // end if
                else {
-                  printf( "%d not found.\n\n", item );
+                  printf( "%d not found.\n\n", item);
                } // end else
             } // end if
             else {
@@ -54,5 +56,6 @@ int main( void )
       scanf( "%u", &choice );
    } // end while
   /* Clear all nodes at the end of nodes*/
-   puts( "End of run." );
+  clear_all_nodes( startPtr );
+  puts( "End of run." );
 } // end main
